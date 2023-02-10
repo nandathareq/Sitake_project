24ce40a2ee131782f47a250bc556f78ee3204d05
nandathareq <nanda.thareq@gmail.com>
2023-02-10 11:18:14 +0700

    create login register and splash screen

---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kBackgroundColor = Color(0xFFD2FFF4);
+const kPrimaryColor = Color(0xFF2D5D70);
+const kSecondaryColor = Color(0xFF265DAB);
+
+const String logo = "lib/assets/images/logo.png";
+
+const String loading = "lib/assets/animations/loading.json";
---
+++ lib/widgets/login_content.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+import 'animations/change_screen_animation.dart';
+import '../utils/constants.dart';
+import '../utils/helper_functions.dart';
+import 'bottom_text.dart';
+import 'top_text.dart';
+
+enum Screens {
+  createAccount,
+  login,
+}
+
+class LoginContent extends StatefulWidget {
+  const LoginContent({Key? key}) : super(key: key);
+
+  @override
+  State<LoginContent> createState() => _LoginContentState();
+}
+
+class _LoginContentState extends State<LoginContent>
+    with TickerProviderStateMixin {
+  late final List<Widget> createAccountContent;
+  late final List<Widget> loginContent;
+  bool isTextFieldFocus = false;
+
+  Widget inputField(
+      String hint, IconData iconData, bool obscure, TextInputType keyboard) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 8),
+      child: SizedBox(
+        height: 50,
+        child: Material(
+          elevation: 8,
+          shadowColor: Colors.black87,
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(30),
+          child: TextField(
+            keyboardType: keyboard,
+            obscureText: obscure,
+            textAlignVertical: TextAlignVertical.bottom,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              hintText: hint,
+              prefixIcon: Icon(iconData),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget loginButton(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 135, vertical: 16),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 14),
+          shape: const StadiumBorder(),
+          primary: kSecondaryColor,
+          elevation: 8,
+          shadowColor: Colors.black87,
+        ),
+        child: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget forgotPassword() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 110),
+      child: TextButton(
+        onPressed: () {},
+        child: const Text(
+          'Lupa Password?',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: kSecondaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    createAccountContent = [
+      inputField(
+          'Nama Pengguna', Ionicons.person_outline, false, TextInputType.name),
+      inputField('Nomor Induk Karyawan', Ionicons.card_outline, false,
+          TextInputType.number),
+      inputField(
+          'Password', Ionicons.lock_closed_outline, true, TextInputType.text),
+      loginButton('Buat Akun'),
+    ];
+
+    loginContent = [
+      inputField('Nomor Induk Karyawan', Ionicons.card_outline, false,
+          TextInputType.number),
+      inputField(
+          'Password', Ionicons.lock_closed_outline, true, TextInputType.text),
+      loginButton('Login'),
+      forgotPassword(),
+    ];
+
+    ChangeScreenAnimation.initialize(
+      vsync: this,
+      createAccountItems: createAccountContent.length,
+      loginItems: loginContent.length,
+    );
+
+    for (var i = 0; i < loginContent.length; i++) {
+      loginContent[i] = HelperFunctions.wrapWithAnimatedBuilder(
+        animation: ChangeScreenAnimation.loginAnimations[i],
+        child: loginContent[i],
+      );
+    }
+
+    for (var i = 0; i < createAccountContent.length; i++) {
+      createAccountContent[i] = HelperFunctions.wrapWithAnimatedBuilder(
+        animation: ChangeScreenAnimation.createAccountAnimations[i],
+        child: createAccountContent[i],
+      );
+    }
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    ChangeScreenAnimation.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const Positioned(
+          top: 136,
+          left: 24,
+          child: TopText(),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: loginContent,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: createAccountContent,
+              ),
+            ],
+          ),
+        ),
+        const Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 50),
+            child: BottomText(),
+          ),
+        ),
+      ],
+    );
+  }
+}
